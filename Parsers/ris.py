dd09356174778d24d83c2f76c030b713c429e800
John Wallis <eviper01@gmail.com>
2023-05-29 16:34:11 +0800

    Added Ris Parser and made progress on implementing backbone features

--- Parsers/ris.py
+++ Parsers/ris.py
@@ -1,2 +1,48 @@
+def Identity(value):
+    return value[0]
+
+def Authours(authour_list):
+    return " and ".join(authour_list)
+
+def Dates(date):
+    return date[0][0:4]
+
+def InText(params):
+    Authour = params[0]
+    Authour = Authour.split(",")[1][1]+Authour.split(',')[0]
+    date = params[1]
+    date = date[2:4]
+    return Authour+date
+
 def parse(text):
-    print("parsing ris files")
+    """Parses .ris files native to endnote"""
+    lines = text.split("\n")
+    data = []
+    #get key values
+    for line in lines:
+        key = line.split("-")[0].strip()
+        value = line.split("-")[1].strip()
+        data.append([key,value])
+    #convert key values to others
+    #translator is a list of output file key, data to get, data to pass.
+    translations = [
+                        ["InText",  ["A1", "PY"],   InText],
+                        ["Authour", ["A1", "AU"],   Authours],
+                        ["Title",   ["TI"],         Identity],
+                        ["Journal", ["JF"],         Identity],
+                        ["Volume",  ["VL"],         Identity],
+                        ["Number",  ["IS"],         Identity],
+                        ["Pages",   ["SP"],         Identity],
+                        ["Year",    ["PY"],         Dates],
+                        ["DOI",     ["DO"],         Identity]
+                    ]
+    parsed = {}
+    for key_set in translations:
+        key = key_set[0]
+        param_list = []
+        for id in key_set[1]:
+            param_list += [dataline[1] for dataline in data if dataline[0] == id]
+        value = key_set[2](param_list)
+        parsed[key] = value
+    return parsed
+                
--- main.py
+++ main.py
@@ -1,30 +1,168 @@
 import sys
 import os
 import glob
-from FileParsing import ParseReference
+import pprint
+import json
+import importlib
+import sys
+sys.path.append(__file__+'/../Parsers/')
+
+#Global Variables
+db = None
+tags = []
+bib_file = None
+ref_db = None
+
+
+
+def LoadParsers():
+    """Checks in the /Parsers/ Directory for the avalible {ext}.py Parsers"""
+    parsers = glob.glob(__file__+'/../Parsers/*') # * means all if need specific format then *
+    return [parser.split("\\")[-1].split(".")[0] for parser in parsers] 
+
+def ParseReference(text, filename):
+    """Passes the text in the ref file to the relvant parser returns in dictionary of key value pairs that have the same names and formatting as bibtex however 'ref' is used to denote the refernce identifier """
+    extension = filename.split(".")[-1]
+    parsers = LoadParsers()
+    if extension in parsers:
+        parser = importlib.import_module(extension)
+        return parser.parse(text)
+    print("ParseRefernce: parser not found")
+    return None
+
+
+
+
+
+
+
+
+
+def LoadConfig():
+    global db
+    global tags
+    global bib_file
+    MissingConfig = False
+    """Loads the config from the config.json file which is in the same directory as main.py"""
+    try:
+        with open(__file__+'/../config.json',"r") as file:
+            config = json.load(file)
+    except FileNotFoundError:
+        print("config.json file not found, will create one instead")
+        MissingConfig = True
+    try:
+        db = config['db']
+    except KeyError:
+        print('database pointer missing defaulting to', __file__+'/../db.json')
+        db = __file__+'/../config.json'
+    except UnboundLocalError:
+        db = __file__+'/../config.json'
+    try:
+        tags = config['tags']
+    except KeyError:
+        print("Could not find any working tags")
+        tags = []
+    except UnboundLocalError:
+        tags = []
+    try:
+        bib_file = config['bib_file']
+    except KeyError:
+        print("Could not find pointer to bib file:")
+        print("defaulting to", __file__+'/../bibfile.bib')
+        bib_file = __file__+'/../bibfile.bib'
+    except UnboundLocalError:
+        bib_file = __file__+'/../bibfile.bib' 
+        print("bib file defaulting to", __file__+'/../bibfile.bib') 
+    if MissingConfig:
+        with open(__file__+'/../config.json',"w") as file:
+            config = {
+                        'db': db,
+                        'tags': tags,
+                        'bib_file': bib_file 
+                    }
+            file.write(json.dumps(config, indent = 4))
+
+def SaveConfig():
+    with open(__file__+'/../config.json',"w") as file:
+        config = {
+                    'db': db,
+                    'tags': tags,
+                    'bib_file': bib_file
+                }
+        file.write(json.dumps(config, indent = 4))
+
+def LoadDatabase():
+    try:
+        with open (db,'r') as file:
+            return json.loads(file.read()) 
+    except FileNotFoundError:
+        print("LoadDataBase: cannot find database file, will start a new one.")
+        return {}
+
+def WriteDatabase():
+    with open (db,'w') as file: 
+        file.write(json.dumps(ref_db, indent = 4))
+
+def SaveReferences(references_list):
+    global ref_db
+    for reference in references_list:
+        reference['tags'] = tags
+    if reference['InText'] not in ref_db.keys():
+        ref_db[reference['InText']] = reference
+    else:
+        print("SaveReferences: Warning: potential duplicate reference found")
+        print("Trying to Save:")
+        pp = pprint.PrettyPrinter(indent=4, width=200)
+        print("Old Reference:")
+        pp.pprint(ref_db[reference['InText']])
+        print("New Reference:")
+        pp.pprint(reference)
+        #run something to handle duplicates
+        handling = True
+        while handling:
+            action = input("'o' to overwrite, 'd' to discard new reference, 'n' to manually specify name: ")
+            if action == 'o':
+                ref_db[reference['InText']] = reference
+                handling = False
+            if action == 'd':
+                handling = False
+            if action == 'n':
+                name = input("Specify name: ")
+                ref_db[name]= reference
+                handling = False
+            if handling: 
+                print("Invalid option")
+
 
 def OpenReference(file):
+    """opens and parsers the specifed file """
     try:
         with open(file, "r") as refFile:
             refData = refFile.read()
-            ParseReference(refData, file)
+            SaveReferences([(ParseReference(refData, file))])
     except FileNotFoundError:
-        print("Invalid File Name:{Name} File not Found".format(Name=sys.argv[2]))
+        print("OpenRefernce: Invalid File Name:{Name} File not Found".format(Name=sys.argv[2]))
 
 def QuickAdd(directory):
+    """attempts to parse the newest file in the specified directory"""
     list_of_files = glob.glob('{dir}/*'.format(dir=directory)) # * means all if need specific format then *
     latest_file = max(list_of_files, key=os.path.getctime)
     OpenReference(latest_file)
 
 def AddAll(directory):
+    """attempts to parse every file in the specified directory"""
     list_of_files = glob.glob('{dir}/*'.format(dir=directory)) # * means all if need specific format then *
     for file in list_of_files:
         OpenReference(file)
 
 
-
+LoadConfig()
 if __name__ == "__main__":
+    ref_db = LoadDatabase()
     command = sys.argv[1]
+
+    ##write a command parser that can understand -- and - commands
+
     if command in ["import", "add"]:
         if len(sys.argv[2:]) == 0:
             print("No files specified to import")
@@ -37,3 +175,31 @@ if __name__ == "__main__":
             print("Please specify a directory to import from")
     if command in ["import-all", "addall"]:
         AddAll(sys.argv[2])
+    
+    
+    if command in ["set-db"]:
+        try: 
+            db = sys.argv[2]
+            SaveConfig()
+        except IndexError:
+            print("Please specify a file path to the reference database")
+    if command in ["set-tag"]:
+        tags = sys.argv[2:]
+        SaveConfig()
+    if command in ["set-bib"]:
+        try:
+            bib_file = sys.argv[2]
+            SaveConfig
+        except IndexError:
+            print("Please specify a file path to the working bibliography")
+    
+    
+    if command in ["tag"]:
+        tags = sys.argv[2:]
+        SaveConfig()
+    if command in ["export", "ex"]:
+        print("sepcify tags to export: appends to file")
+    if command in ["read"]:
+        print("use the doi to get the pdf of the file")
+    
+    WriteDatabase()
